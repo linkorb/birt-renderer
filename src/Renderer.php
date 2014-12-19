86900026f0e8d9a4c6e1ee6520a6f175bcb0278d
j.faassen <j.faassen@linkorb.com>
2014-12-19 13:29:21 +0100

    Dynamically resolve format by outputfilename. Adds parameters

--- src/Renderer.php
+++ src/Renderer.php
@@ -2,10 +2,40 @@
 
 namespace BirtRenderer;
 
+use RuntimeException;
+
 class Renderer
 {
-    public function render(Report $report, $parameters, $outputfilename)
+    public function render(Report $report, $parameters, $outputfilename, $options)
     {
+        $format = null;
+        $outputextension = pathinfo($outputfilename, PATHINFO_EXTENSION);
+        if (!$format) {
+            switch ($outputextension) {
+                case 'pdf':
+                    $format = 'pdf';
+                    break;
+                case 'html':
+                    $format = 'html';
+                    break;
+                case 'doc':
+                    $format = 'doc';
+                    break;
+                case 'xls':
+                    $format = 'xls';
+                    break;
+                case 'ppt':
+                    $format = 'ppt';
+                    break;
+                case 'postscript':
+                case 'ps':
+                    $format = 'postscript';
+                    break;
+                default:
+                    throw new RuntimeException("Can't auto-determine format by outputfilename extension. Please pass specifically in the options, or use a recognised filename extension.");
+                    break;
+            }
+        }
         if (!file_exists($this->birthome)) {
             throw new RuntimeException("Undefined or invalid BIRT_HOME: " . $this->birthome);
         }
@@ -14,13 +44,17 @@ class Renderer
             throw new RuntimeException("Script not found: " . $scriptfilename);
         }
         $cmd = $scriptfilename;
-        $cmd .= " -p \"StockLocation=Customer\"";
-        $cmd .= " -f pdf -o " . $outputfilename;
+        foreach ($parameters as $parameter) {
+            $cmd .= " --parameter \"" . $parameter->getKey() . "=" . $parameter->getValue() . "\"";
+        }
+        
+        $cmd .= " -f " . $format;
+        $cmd .= " -o " . $outputfilename;
         $cmd .= " --file " . $report->getFilename();
         
         echo $cmd . "\n";
         putenv('BIRT_HOME=' . $this->birthome);
-        exec($cmd);
+        exec($cmd, $output);
     }
     
     public function setBirtHome($birthome)
